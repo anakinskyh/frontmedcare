7f8363d55f74d1f1373f1bd4331fffe207d63209
anakinskyh <anakin.sky@protonmail.com>
2016-12-03 22:17:30 +0700

    just init

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -11,6 +11,11 @@ app.config(function($routeProvider) {
         	templateUrl : `/assets/html/signin.html`,
 			controller  : `signinController`
 		})
+
+		.when('/patient/makeappt', {
+        	templateUrl : `/assets/html/patient.html`,
+			controller  : `patientMakeApptController`
+		})
 		
 		.when('/patient', {
         	templateUrl : `/assets/html/patient.html`,
@@ -23,4 +28,11 @@ app.controller('signinController', function($scope) {
 
         // create a message to display in our view
         $scope.name = 'anakin skywalker';
+});
+
+app.controller('patientMakeApptController', function($scope) {
+	console.log('load patient');
+	//$scope.sometext = "I'm good.";
+	$scope.activepath = 'assets/html/patientMakeAppt.html';
+
 });
